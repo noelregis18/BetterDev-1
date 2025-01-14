dda3851aa6bdcfc3243440ed370328d9c0c72d7c
poswalsameer <poswalsameer2003@gmail.com>
2025-01-14 23:33:30 +0530

    change in eslint 2

--- betterdev/eslint.config.mjs
+++ betterdev/eslint.config.mjs
@@ -16,6 +16,7 @@ const eslintConfig = [
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-explicit-any": "off", 
       "@typescript-eslint/no-require-imports": "off",
+      "@typescript-eslint/ban-ts-comment": off,
     },
   },
 ];
